2ac984d110052964ed95d7315f2ead205c7cda76
TRAORE LACINA <149535272+Traorelacina@users.noreply.github.com>
2025-06-29 19:13:25 +0000

    Update client.tsx

--- src/api/client.tsx
+++ src/api/client.tsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 const client = axios.create({
   baseURL: 'https://ecommerce-backend-2-12tl.onrender.com/api',
   withCredentials: true,
-  timeout: 10000, // 10 secondes timeout
+  timeout: 50000, // 10 secondes timeout
 });
 
 // Intercepteur pour les requêtes
